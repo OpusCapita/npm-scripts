93d1ff8024db4b4db2e47a11886d6de50a16db81
baliunov <baliunov@scand.com>
2017-08-09 15:49:19 +0300

    delete commits/tags which have been created if npm publishing into repo fails

--- bin/npm-publish.js
+++ bin/npm-publish.js
@@ -4,8 +4,36 @@ var program = require('commander');
 var fluidPublish = require('fluid-publish');
 var path = require('path');
 var fs = require('fs');
-var lodash = require('lodash');
 var execSync = require("child_process").execSync;
+
+
+function isTag(tag) {
+  const tagList = execSync('git tag').toString().split('\n');
+
+  for (let i = 0; i < tagList.length; i++) {
+    if (tagList[i] === tag) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function isNpmVersion(packageName, version) {
+  const npmVersion = execSync(`npm view ${packageName} version`).toString();
+  return npmVersion.trim() === version.trim();
+}
+
+function cleanErrorPublish({ packageName, hasTag, hasVersion, tag, version}) {
+  if (hasTag) { // Delete tag from GitHub
+    execSync(`git tag -d ${tag}`);
+    execSync(`git push origin :refs/tags/${tag}`);
+  }
+
+  if (hasVersion) { // Delete version from npm
+    execSync(`npm unpublish ${packageName}@${version}`);
+  }
+}
 
 program
   .version(require('../package.json').version)
@@ -25,7 +53,7 @@ program.parse(process.argv);
 
 //override default date format and change timestamp of current revision to now time
 fluidPublish.convertToISO8601 = function(timestamp) {
-  var date = new Date().toISOString();
+  const date = new Date().toISOString();
   //remove {':', '-'} symbols from format because npm publish doesn't work
   return date.split(':').join('').split('-').join('');
 };
@@ -34,28 +62,42 @@ if (program.release) {
   var updatingVersionMessage = 'Updating to a version to ';
   var currentPath = process.cwd();
   var packageFilename = path.join(currentPath, 'package.json');
-  var version = require(packageFilename).version;
+  var currentPackage = require(packageFilename);
+  var version = currentPackage.version;
+  var tag = `v${version}`;
+  var packageName = currentPackage.name;
+  var isSuccessful = true;
 
-  if (!program.test) {
-    execSync(`node ${path.resolve(__dirname, './update-changelog.js')}`, { stdio: 'inherit' });
-    execSync('git push');
+  try {
+    fluidPublish.standard(program.test, {
+      "pushVCTagCmd": "git push origin v${version}",
+      "vcTagCmd": "git tag -a v${version} -m \"Tagging the ${version} release\""
+    });
+  } catch (e) {
+    isSuccessful = false;
+
+    if (!program.test) {
+      const hasTag = isTag(tag);
+      const hasVersion = isNpmVersion(packageName, version);
+      cleanErrorPublish({ packageName, hasTag, hasVersion, tag, version});
+
+      process.exit(1);
+    }
   }
 
-  fluidPublish.standard(program.test, {
-    "pushVCTagCmd": "git push origin v${version}",
-    "vcTagCmd": "git tag -a v${version} -m \"Tagging the ${version} release\""
-  });
+  if (!program.test && isSuccessful) {
+    execSync(`node ${path.resolve(__dirname, './update-changelog.js')}`, { stdio: 'inherit' });
+    execSync('git push');
 
-  if (!program.test) {
-    var vNumbers = version.split(".");
-    var lastNumber = parseInt(vNumbers[vNumbers.length - 1], 10);
+    let vNumbers = version.split(".");
+    let lastNumber = parseInt(vNumbers[vNumbers.length - 1], 10);
     vNumbers[vNumbers.length - 1] = lastNumber + 1;
-    var targetVersion = vNumbers.join('.');
+    let targetVersion = vNumbers.join('.');
 
-    var VERSION_REGEXP = new RegExp(
+    let VERSION_REGEXP = new RegExp(
       '([\'|\"]?version[\'|\"]?[ ]*:[ ]*[\'|\"]?)[\\d||A-a|.|-]*([\'|\"]?)', 'gi');
 
-    var packageData = fs.readFileSync(packageFilename).toString();
+    let packageData = fs.readFileSync(packageFilename).toString();
     packageData = packageData.replace(VERSION_REGEXP, "\"version\": \"" + targetVersion + "\"");
     fs.writeFileSync(packageFilename, packageData);
 
